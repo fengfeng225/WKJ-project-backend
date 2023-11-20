db900e7ce6c913e637f205cac165c75573398800
郭贺男 <f603252728@163.com>
2023-11-20 17:25:04 +0800

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql', // 连接mysql数据库
+      host: 'localhost',
+      port: 3306,
+      username: '用户名',
+      password: '数据密码',
+      database: '数据库名',
+      // 自动加载实体
+      autoLoadEntities: true,
+      // 手动导入
+      // entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('防互窜管理系统')
+    .setDescription('防互窜管理系统项目后端搭建')
+    .setVersion('1.0')
+    .build();
+
+  const documents = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, documents);
+
   await app.listen(9000);
 }
 bootstrap();
