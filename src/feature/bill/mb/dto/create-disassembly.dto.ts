866a99ac8fd9d5bd13f40990eda670f46f6da5d7
郭贺男 <f603252728@163.com>
2023-11-28 17:23:04 +0800

    add entity

---
+++ src/feature/bill/mb/dto/create-disassembly.dto.ts
@@ -0,0 +1,131 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
+
+export class CreateDisassemblyDto {
+  @ApiProperty({
+    example:"number",
+    description:"所属班组Id"
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  classId: number;
+
+  @ApiProperty({
+    example:"string",
+    description:"装置名称"
+  })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    example:"string",
+    description:"编号"
+  })
+  @IsNotEmpty()
+  @IsString()
+  code: string;
+
+  @ApiProperty({
+    example:"string",
+    description:"状态"
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  status: number;
+
+  @ApiProperty({
+    example:"string",
+    description:"管径"
+  })
+  @IsNotEmpty()
+  @IsString()
+  pipDiameter: string;
+
+  @ApiProperty({
+    example:"string",
+    description:"盲板安装位置描述"
+  })
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @ApiProperty({
+    example:"number",
+    description:"管线介质名称"
+  })
+  @IsNotEmpty()
+  @IsString()
+  pipelineMediaName: string;
+
+  @ApiProperty({
+    example:"string",
+    description:"管线介质温度"
+  })
+  @IsNotEmpty()
+  @IsString()
+  pipelineMediaTemperature: string;
+
+  @ApiProperty({
+    example:"string",
+    description:"管线介质压力"
+  })
+  @IsNotEmpty()
+  @IsString()
+  pipelineMediaPressure: string;
+
+  @ApiProperty({
+    example:"string",
+    description:"盲板规格"
+  })
+  @IsNotEmpty()
+  @IsString()
+  size: string;
+
+  @ApiProperty({
+    example:"string",
+    description:"盲板形式"
+  })
+  @IsNotEmpty()
+  @IsString()
+  type: string;
+
+  @ApiProperty({
+    example:"string",
+    description:"盲板材质"
+  })
+  @IsNotEmpty()
+  @IsString()
+  material: string;
+
+  @ApiProperty({
+    example:"string",
+    description:"拆装时间"
+  })
+  @IsNotEmpty()
+  disassembleTime: Date;
+
+  @ApiProperty({
+    example:"string",
+    description:"操作人员"
+  })
+  @IsNotEmpty()
+  @IsString()
+  operator: string;
+
+  @ApiProperty({
+    example:"string",
+    description:"管理干部"
+  })
+  @IsNotEmpty()
+  @IsString()
+  manager: string;
+
+  @ApiProperty({
+    example:"number",
+    description:"表示删除"
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  deleteMark: number;
+}
---
+++ src/feature/bill/mb/entities/mb-disassembly.entity.ts
@@ -0,0 +1,121 @@
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn
+} from 'typeorm';
+import { MbClass } from './mb-class.entity';
+
+@Entity()
+export class MbDisassembly {
+  @PrimaryGeneratedColumn({ comment: '自然主键' })
+  id: number;
+
+  @Column({
+    length: 50,
+    comment: '装置名称'
+  })
+  name: string;
+
+  @Column({
+    length: 50,
+    comment: '盲板编号'
+  })
+  code: string;
+
+  @Column({
+    length: 50,
+    comment: '管径'
+  })
+  pipDiameter: string;
+
+  @Column({
+    type: 'text',
+    comment: '盲板安装位置描述'
+  })
+  description: string;
+
+  @Column({
+    type: 'int',
+    comment: '盲通状态'
+  })
+  status: number;
+
+  @Column({ type: 'timestamp', comment: '拆装时间' })
+  disassembleTime: Date;
+
+  @Column({
+    length: 50,
+    comment: '管线介质名称'
+  })
+  pipelineMediaName: string;
+
+  @Column({
+    length: 50,
+    comment: '管线介质温度 (℃)'
+  })
+  pipelineMediaTemperature: string;
+
+  @Column({
+    length: 50,
+    comment: '管线介质压力 (MPa)'
+  })
+  pipelineMediaPressure: string;
+
+  @Column({
+    length: 50,
+    comment: '盲板规格 (mm)'
+  })
+  size: string;
+
+  @Column({
+    length: 50,
+    comment: '盲板形式'
+  })
+  type: string;
+
+  @Column({
+    length: 50,
+    comment: '盲板材质'
+  })
+  material: string;
+
+  @Column({
+    length: 50,
+    comment: '操作人员'
+  })
+  operator: string;
+
+  @Column({
+    length: 50,
+    comment: '管理干部'
+  })
+  manager: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(0)', comment: '该记录产生时间', select: false })
+  creatorTime: Date;
+
+  @Column({
+    type: 'int',
+    comment: '表示删除'
+  })
+  deleteMark: number;
+
+  @Column({
+    length: 50,
+    comment: '拆装操作说明'
+  })
+  remark: string;
+
+  @Column({
+    length: 50,
+    comment: '盲板所属周期'
+  })
+  cycleType: string;
+
+  @Column({comment: '所属班组ID'})
+  classId: number;
+
+  @ManyToOne(() => MbClass, {cascade: true})
+  class: MbClass;
+}
