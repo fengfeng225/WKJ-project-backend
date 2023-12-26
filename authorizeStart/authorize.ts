c0465c57fb513030241d53f54027e5be1a9dd29c
郭贺男 <f603252728@163.com>
2023-12-26 09:01:03 +0800

    add a systeminfo authorize

--- authorizeStart/authorize.ts
+++ authorizeStart/authorize.ts
@@ -4,6 +4,7 @@ async function authorize() {
   const uuidInfo = await si.uuid()
   const diskInfo = await si.diskLayout()
   const serialKey = diskInfo[0].serialNum.split('_').map(v => v[0]).join('')
+  // console.log(uuidInfo, serialKey)
   if (uuidInfo.macs[0] === 'f8:8c:21:c2:8c:fc' || serialKey === 'A297') return true
 }
 
